e05fc61c682a40e811414a697ab866e2fcbffdd8
steveng-1226 <steveng1226@gmail.com>
2017-10-07 18:32:14 -0700

    added router support for events
    yeah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EventsComponent } from './events/events.component';
 import { HomeComponent } from './home/home.component';
+const ROUTES=[
+{
+  path: "events/velocity-vortex", 
+  component: EventsComponent
+}]
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
