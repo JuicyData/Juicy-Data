5f3b08ab40137002b0d3326f95bdf5b20a0b5294
Ryan N <ryguyn@gmail.com>
2017-10-28 16:53:52 -0700

    Added API route for inserting game data

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,25 @@
 var MongoClient = require('mongodb').MongoClient
+var url = "mongodb://localhost:27017/TheOrangeAlliance";
 // var configDB = require('./../config/database.js')
 module.exports = function(app) {
-	//require('./module')(app)
+	app.get('/api', function(req, res) {
+		res.send('api home')
+	})
+
+	app.post('/api/insert-gamedata', function(req, res) {
+		MongoClient.connect(url, function(err, db) {
+			if (err) {
+				res.status(500).send(err)
+			} else {
+				db.collection("gameData").insertOne(req.body, function(err, result) {
+					if (err) {
+						res.status(500).send(err)
+					} else {
+						console.log("Game Data Inserted");
+						db.close();	
+					}
+				});
+			}
+		});
+	})
 }
