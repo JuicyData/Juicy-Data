f10c6af8148300e43c649e3e05018230d4a6e730
Michael Leonffu <leonffu.michael@gmail.com>
2017-11-02 03:13:09 -0700

    Routes was annoying to me.

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,8 @@
-var MongoClient = require('mongodb').MongoClient
-var configDB = require('./../config/database.js')
+//Main router for all the API; consider this API hub
+//var MongoClient = require('mongodb').MongoClient
+//var configDB = require('./../config/database.js')
 module.exports = function(app) {
 	//require('./module')(app)
+	//require('./cleaning')(app)
+	//require('./orangePeeler')(app)
 }
