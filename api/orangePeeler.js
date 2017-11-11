3958dbb6f9aeda0e58ce44eef8816c94192bba94
Michael Leonffu <leonffu.michael@gmail.com>
2017-11-11 12:57:10 -0800

    Upgradted orangePeeler

--- api/orangePeeler.js
+++ api/orangePeeler.js
@@ -1,4 +1,4 @@
-//API orangePeeler by Michael Leonffu
+//orangePeeler by Michael Leonffu
 var MongoClient = require('mongodb').MongoClient
 //var configDB = require('./../config/database.js')
 //ObjectId = require('mongodb').ObjectID
@@ -10,9 +10,9 @@ var juicyCalculator = require('./juicyCalculator')
 //orangePeeler males sure to clean and sort the differnt 
 //oranges before processing them into juicyData in order to achive best quality juicyData
 
-function teamInfluencePeeler(scheduleCollection, matchDataCollection){
+function teamOffensiveInfluencePeeler(scheduleCollection, matchDataCollection, callback){
 	//This make sure to find the oranges that are important for calculating OPR; schedule and matchdata
-	console.log('[START]-teamInfluencePeeler')
+	console.log('[START]-teamOffensiveInfluencePeeler')
 	MongoClient.connect('mongodb://localhost/TheOrangeAllianceTest', function(err,db){
 		//If there is an error while connecting to the database
 		if(err){
@@ -123,21 +123,148 @@ function teamInfluencePeeler(scheduleCollection, matchDataCollection){
 				// 	]
 				// }
 
-				console.log('Operation teamInfluencePeeler time(Milliseconds):',new Date(new Date()-peelerTimer).getMilliseconds())
-				console.log('[DONE]-teamInfluencePeeler')
+				console.log('Operation teamOffensiveInfluencePeeler time(Milliseconds):',new Date(new Date()-peelerTimer).getMilliseconds())
+				console.log('[DONE]-teamOffensiveInfluencePeeler')
 				db.close()
-				juicyCalculator(peeledOrange)
-				return peeledOrange
+				callback(peeledOrange)
+			}
+		}
+	})
+}
+
+function teamMarginalInfluencePeeler(scheduleCollection, matchDataCollection, callback){
+	//This make sure to find the oranges that are important for calculating OPR; schedule and matchdata
+	console.log('[START]-teamMarginalInfluencePeeler')
+	MongoClient.connect('mongodb://localhost/TheOrangeAllianceTest', function(err,db){
+		//If there is an error while connecting to the database
+		if(err){
+			console.log(err)
+			return
+		}
+
+		//Make the schedule and matchData.....
+		db.collection(scheduleCollection).aggregate([
+			{$unwind:'$schedule'},
+			{$lookup:{
+				from:matchDataCollection,
+				localField:'schedule.match',
+				foreignField:'matchInformation.matchNumber',
+				as:'matchData'
+			}},
+			{$unwind:'$matchData'},
+			{$project:{
+				_id:0,
+				teamsScore:[
+					{
+						//teams:['$schedule.teams.red1','$schedule.teams.red2'],
+						team1:'$schedule.teams.red1',
+						team2:'$schedule.teams.red2',
+						score:{$subtract:['$matchData.resultsInformation.score.total.red', '$matchData.resultsInformation.score.total.blue']}
+					},
+					{
+						//teams:['$schedule.teams.blue1','$schedule.teams.blue2'],
+						team1:'$schedule.teams.blue1',
+						team2:'$schedule.teams.blue2',
+						score:{$subtract:['$matchData.resultsInformation.score.total.blue', '$matchData.resultsInformation.score.total.red']}
+					}
+				]
+			}},
+			{$unwind:'$teamsScore'},
+			{$facet:{
+				teamsScore:[
+					{$project:{
+						//teams:'$teamsScore.teams',
+						teams:['$teamsScore.team1','$teamsScore.team2'],
+						score:['$teamsScore.score']
+					}
+				}],
+				teamList:[
+					{$group:{
+						_id:'Anna Li',
+						teamsList:{$addToSet:'$teamsScore.team1'},
+						teamsList:{$addToSet:'$teamsScore.team2'}
+					}}
+				]
+			}},
+			{$unwind:'$teamList'},
+			{$project:{
+				teamsScore:1,
+				teamsList:'$teamList.teamsList'
+			}}
+		], teamsScores)
+
+		// {
+		// 	teamsScore: [
+		// 		{
+		// 			teams:[123,123], Team numbers
+		// 			score:[123] score these teams got
+		// 		}
+		// 	],
+		// 	teamList:[123,123,123,] Team numbers unique list
+		// }
+
+
+		//Callback; the function that runs after the dataase gives a responce
+		function teamsScores(err, docs){
+			if(err){
+				console.log(err)
+				db.close()
+			}else if(!(0 in docs)){
+				console.log('Failed to get docs')
+				db.close()
+			}else{
+				var doc = docs[0] //There should be one doc in docs
+				//If threre are no errors then:
+				var peeledOrange = {
+					labels: doc.teamsList.sort(),
+					juice: [],
+					result: []
+				}
+
+				var peelerTimer = new Date()
+
+				for (var i = doc.teamsScore.length - 1; i >= 0; i--) {
+					peeledOrange.result.push(doc.teamsScore[i].score)
+					peeledOrange.juice[i] = []
+					for (var j = peeledOrange.labels.length - 1; j >= 0; j--) {
+						peeledOrange.juice[i][j] = doc.teamsScore[i].teams[0] == peeledOrange.labels[j] || doc.teamsScore[i].teams[1] == peeledOrange.labels[j]?1:0
+					}
+				}
+				peeledOrange.result.reverse() //It's backwards?
+
+				// returns it as 
+				// {
+				// 	labels:[],
+				// 	juice:[
+				// 		[],
+				// 		[]
+				// 	],
+				// 	result:[
+				// 		[],
+				// 		[]
+				// 	]
+				// }
+
+				console.log('Operation teamMarginalInfluencePeeler time(Milliseconds):',new Date(new Date()-peelerTimer).getMilliseconds())
+				console.log('[DONE]-teamMarginalInfluencePeeler')
+				db.close()
+				callback(peeledOrange)
 			}
 		}
 	})
 }
 
 
-//teamInfluencePeeler('scheduleturing', 'matchDataturing')
-teamInfluencePeeler('scheduleeuclid', 'matchDataeuclid')
+//teamOffensiveInfluencePeeler('scheduleturing', 'matchDataturing')
+//teamOffensiveInfluencePeeler('scheduleeuclid', 'matchDataeuclid')
+//teamMarginalInfluencePeeler('scheduleeuclid', 'matchDataeuclid')
+//teamMarginalInfluencePeeler('scheduleturing', 'matchDataturing', callback)
 
+function callback(peeledOrange){
+	console.log(peeledOrange)
+}
 
-// module.exports = function(app) {
-
-// }
+module.exports = {
+	teamOffensiveInfluencePeeler: teamOffensiveInfluencePeeler,
+	teamMarginalInfluencePeeler: teamMarginalInfluencePeeler
+}
