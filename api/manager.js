48c6424d35afc083aa83b3dd49366bcb1ac67398
Michael Leonffu <leonffu.michael@gmail.com>
2018-01-08 16:27:18 -0800

    Kinda started manager

--- api/manager.js
+++ api/manager.js
@@ -1,5 +1,27 @@
 //manager by Michael Leonffu
 
-This is the main script that'll run all the other scripts. This is what you'd call manager.js
+// This is the main script that'll run all the other scripts. This is what you'd call manager.js
+// This script checks what to do (which events to update) and how to do them.
+// This will also be a hub
 
-This script checks what to do (which events to update) and how to do them.
+var MongoClient = require('mongodb').MongoClient
+var configDB = require('./../config/database')
+var ObjectId = require('mongodb').ObjectID
+//var apiKey = require('./../config/apiKey')
+
+module.exports = {}
+
+MongoClient.connect(configDB.url, function(err,db){
+	if(err){
+		console.log(err)
+		res.status(500).send(err)
+		return
+	}else{
+
+		var orangeFarm = require('./orangeFarm/orangeFarm') // load our routes and pass in our app
+		orangeFarm({db:db, ObjectId:ObjectId}, '1718-NCAL-RWC', function(farmReport){
+			console.log(farmReport)
+			db.close()
+		})
+	}
+})
