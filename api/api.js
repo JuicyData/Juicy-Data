7c16157bc8060447db3283f9154f5e81efc6f78a
Michael Leonffu <leonffu.michael@gmail.com>
2018-01-11 16:42:16 -0800

    Added predictions to API

--- api/api.js
+++ api/api.js
@@ -26,6 +26,117 @@ app.get('/api/events/read', (req, res) =>{
 	)
 })
 
+app.get('/api/predict', (req, res) =>{
+
+	var algorithms = require('./algorithms/algorithms')
+
+	// req.query = {
+	// 	eventId: 'abc',
+	// 	algorithm: 'abc',
+	// 	alliance1team1: 123,
+	// 	alliance1team2: 123,
+	// 	alliance2team1: 123,
+	// 	alliance2team2: 123
+	// }
+
+	// res = {
+	// 	prediction: {		//REQUIRED?
+	// 		winner: 'abc',	//Alliance1 or Alliance2
+	// 		chance: .123,	//Chance this is winner in 00.00%	-1 if no value
+	// 		alliance1: {
+	// 			score: .123	//predicted score
+	// 		},
+	// 		alliance2: {
+	// 			score: .123
+	// 		}
+	// 	},
+	// 	model: [	//For every match given
+	// 		{
+	// 			matchNumber: 123,
+	// 			prediction: 'abc',	//red or blue
+	// 			blueScore: 123,
+	// 			redScore: 123,
+	// 			certainty: .123		//as 00.00%; -1 if no value
+	// 		}
+	// 	]
+	// }
+
+	console.log(req.query)
+
+	db.collection('events').aggregate([
+		{$match: {
+			_id: String(req.query.eventId)
+		}},
+		{$lookup:{
+			from: 'matchData',
+			let: {eventToaEventKey: '$_id'},
+			pipeline: [
+				{$match:{$expr:
+					{$eq: ['$_id.toaEventKey', '$$eventToaEventKey']}
+				}},
+				{$project:{
+					_id: 0,
+					matchNumber: '$_id.matchInformation.matchNumber',
+					teams: '$_id.matchInformation.teams',
+					winner: '$resultInformation.winner',
+					score: '$resultInformation.score'
+				}}
+			],
+			as: 'matchData'
+		}},
+		{$lookup:{
+			from: 'gameData',
+			let: {eventToaEventKey: '$_id'},
+			pipeline: [
+				{$match:{$expr:
+					{$eq: ['$_id.toaEventKey', '$$eventToaEventKey']}
+				}},
+				{$project:{
+					_id: 0,
+					matchNumber: '$_id.matchInformation.matchNumber',
+					teams: '_id.matchInformation.teams',
+					gameInformation: '$gameInformation'
+				}}
+			],
+			as: 'gameData'
+		}},
+		{$project:{
+			metaData: {
+				toaEventKey: '$_id',
+				matches: 101
+			},
+			data: {
+				matchData: '$matchData',
+				gameData: '$gameData'
+			}
+		}}
+	], 
+		function(err, eventsDocs){
+			if(err){
+				console.log(err)
+				res.status(500).send(err)
+				return
+			}else{
+				if(eventsDocs){ //NEED TO TEST THIS
+					res.json(algorithms.algorithmLoader('simpleOPR',eventsDocs[0],{
+						alliance1: {
+							team1: Number(req.query.alliance1team1),
+							team2: Number(req.query.alliance1team2)
+						},
+						alliance2: {
+							team1: Number(req.query.alliance2team1),
+							team2: Number(req.query.alliance2team2)
+						},
+					}))
+				}else{		// if threre is no documents returned then:
+					res.status(400).send('Events not found')
+				}
+			}
+		}
+	)
+
+})
+
 app.get('/api/teams/read', (req, res) =>{
 
 	// res = {
