ea7f84533964907bc55c1d1eaafe5739c245b7a2
Ryan Nemiroff <ryguyn@gmail.com>
2018-09-07 23:32:42 -0700

    Log in & log out management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -10,22 +10,19 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'app';
 
-  currentPage = null;
+  currentUrl = null;
 
   constructor(private router: Router, private http: HttpClient) {
     // Track Page Navigation
-    // WARNING: This does not trigger when the user closes the tab
     router.events.forEach((event) => {
       if (event instanceof NavigationEnd) { // Successfully loaded new route
-        let newPage = { url: event.url };
-        // console.log("Entered: " + newPage.url);
+        let newPage = { toUrl: event.url };
+        if (currentUrl) {
+          newPage.fromUrl = currentUrl;
+        }
+        // console.log("Entered: " + newPage.toUrl);
         this.http.post('/api/navigation/enter', newPage).subscribe();
-        this.currentPage = newPage;
-      } else if (event instanceof NavigationStart) { // Navigating away from the current page
-        if (this.currentPage !== null) {
-          // console.log("Left: " + this.currentPage.url);
-          this.http.post('/api/navigation/leave', this.currentPage).subscribe();
-        }        
+        this.currentUrl = newPage.toUrl;
       }
     });
   }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +13,15 @@ export class NavbarComponent implements OnInit {
   showCollapsedNavbar = false;
   width: any;
 
-  constructor() { }
+  signedIn = null;
+
+  constructor(private userService: UserService, private http: HttpClient) {
+  	userService.signedIn.subscribe(signedIn => {
+  		this.signedIn = signedIn;
+  		console.log("signed in!")
+  		console.log(signedIn)
+  	})
+  }
 
   ngOnInit() {
     this.width = window.innerWidth;
@@ -18,6 +29,10 @@ export class NavbarComponent implements OnInit {
 
   navClicked(event) {
   	event.target.parentElement.parentElement.parentElement.classList.remove('show');
+  }
+
+  signOut() {
+  	this.http.get("/api/accounts/accounts/signout").subscribe(() => this.userService.signedIn.next(false));
   }
 
 }
